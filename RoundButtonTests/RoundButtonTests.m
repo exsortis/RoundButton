08cba4777aef043d7f6e676103cebc1e053e8e24
Paul Schifferer <paul@schifferers.net>
2017-10-21 21:11:02 -0700

    Add a unit test.

--- RoundButtonTests/RoundButtonTests.m
+++ RoundButtonTests/RoundButtonTests.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+@import RoundButton;
+
 
 @interface RoundButtonTests : XCTestCase
 
@@ -24,16 +26,9 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testInstantiation {
+    RoundButton* b = [RoundButton new];
+    XCTAssertNotNil(b);
 }
 
 @end
